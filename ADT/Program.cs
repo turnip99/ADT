ec94170715557f7c600e0d2caf436fed5d1cea6b
turnip99 <turnerma101265@aol.com>
2015-11-25 12:26:13 +0000

    Try again.....

--- ADT/Program.cs
+++ ADT/Program.cs
@@ -41,6 +41,9 @@ namespace ADT
             stack.pop();
             Console.WriteLine(stack);
             Console.ReadLine();
+            stack.pop();
+            Console.WriteLine(stack);
+            Console.ReadLine();
         }
     }
 }
