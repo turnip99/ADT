4bc7eeb44035efe189d56d0c353ac86cd0127d33
turnip99 <turnerma101265@aol.com>
2015-11-25 12:24:08 +0000

    First Commit

---
+++ ADT/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(8);
+            stack.push(3);
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.push(9);
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.push(31);
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.push(7);
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.pop();
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.pop();
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.push(3);
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.push(7);
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.pop();
+            Console.WriteLine(stack);
+            Console.ReadLine();
+            stack.pop();
+            Console.WriteLine(stack);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ADT/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    class Stack
+    {
+        private int size;
+        private int[] items;
+        private int pointer;
+        public Stack(int theSize)
+        {
+            size = theSize;
+            pointer = 0; 
+            items = new int[size];
+        }
+        public void push(int newValue)
+        {
+            if (pointer != size)
+            {
+                items[pointer] = newValue;
+                pointer++;
+            }
+            else
+            {
+                Console.WriteLine("Stack is full!");
+            }
+        }
+        public int pop() 
+        {
+            if (pointer > 0)
+            {
+                int temp = items[pointer];
+                items[pointer] = 0;
+                pointer--;
+                return temp;
+	        }
+            else
+            {
+                Console.WriteLine("Stack is empty...");
+                return -1;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder details = new StringBuilder();
+            details.AppendLine("Stack contains");
+            foreach(int item in items)
+            {
+                details.AppendLine(item.ToString());
+            }
+            return details.ToString();
+        }
+    }
+}
